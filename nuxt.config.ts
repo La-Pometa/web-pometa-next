3e58a538ec5291fa1241af8760e11320c3b7e69c
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2022-03-29 12:26:15 +0200

    chore(config): added tailwindcss, srcDir option, prettier and removed unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,23 +9,13 @@ export default defineNuxtConfig({
       },
     ],
   },
+  srcDir: 'src',
   buildModules: [
     '@vueuse/nuxt',
-    '@unocss/nuxt',
+    '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
   ],
   vueuse: {
     ssrHandlers: true,
-  },
-  unocss: {
-    uno: true,
-    attributify: true,
-    preflight: true,
-    icons: {
-      scale: 1.2,
-    },
-    shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ],
   },
 })
---
+++ src/components/Logos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="inline-flex text-2xl font-300 cursor-default">
+    <div class="flex flex-col children:mx-auto">
+      <img class="w-18 h-18 inline-block" src="/nuxt.png" />
+      <span class="text-green5 mt--2">Nuxt 3</span>
+    </div>
+    <div
+      text="3xl gray4"
+      m="x-4 y-auto"
+      transition-all-500
+      transform
+      hover:rotate-135
+      i-carbon-add
+    />
+    <div class="flex flex-col children:mx-auto">
+      <img class="w-18 h-18 inline-block" src="/vite.png" />
+      <span class="text-purple5 mt--2">Vitesse</span>
+    </div>
+  </div>
+</template>
