c22df42327f747d6f8f35484ddabdb8410a32b59
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2022-05-09 11:00:23 +0200

    chore: all custom post types added to content injected class

--- src/plugins/content/content.ts
+++ src/plugins/content/content.ts
@@ -2,11 +2,15 @@ import WPAPI from "wpapi";
 export default class Content extends WPAPI {
 	public path: WPAPI.WPRequestFactory
 	public members: WPAPI.WPRequestFactory
+	public projects: WPAPI.WPRequestFactory
+	public landings: WPAPI.WPRequestFactory
 
 	public constructor(apiUrl: string) {
 		super({ endpoint: apiUrl })
 		this.path = this.registerRoute('wp/v2', '/path')
 		this.members = this.registerRoute('wp/v2', '/member')
+		this.projects = this.registerRoute('wp/v2', '/project')
+		this.landings = this.registerRoute('wp/v2', '/landing')
 	}
 
 }
