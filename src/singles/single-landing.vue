bc296e4e123bb9ff77e8d16742f293e0ea30c952
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2022-05-24 13:32:42 +0200

    refactor: update dearest font

---
+++ src/singles/single-landing.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Post } from '../plugins/content/types'
+
+defineProps<{
+  data: Post
+}>()
+</script>
+<template>
+  <div id="single-landing">
+    <slice-header
+      :params="{
+        title: data.title.rendered,
+        image: data.featured_source,
+        size: 'large',
+        titleOverlay: true,
+      }"
+    ></slice-header>
+    <section class="content" v-html="data.content.rendered"></section>
+  </div>
+</template>
+<style lang="scss">
+.content {
+  @apply container margins-header;
+}
+</style>
