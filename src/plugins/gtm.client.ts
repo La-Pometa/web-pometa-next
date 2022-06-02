55842dd12af033ce733170f33e5a626c5b774a0e
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2022-06-02 09:35:28 +0200

    chore: add gtm as script

--- src/plugins/gtm.client.ts
+++ src/plugins/gtm.client.ts
@@ -1,9 +1,16 @@
-import VueGtag from 'vue-gtag-next'
 
-export default defineNuxtPlugin(({ vueApp: vue }) => {
-	vue.use(VueGtag, {
-		config: {
-			id: 'GTM-N6HVNW4'
-		},
+export default defineNuxtPlugin(() => {
+	useHead({
+		script: [
+			{
+				hid: 'gtm',
+				children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-N6HVNW4');`,
+				type: 'text/javascript'
+			}
+		]
 	})
 })
