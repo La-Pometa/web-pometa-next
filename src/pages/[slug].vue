334a4d0180b65bb5f5dd2377afad81f446f58b4b
Pometa Gràfica <suport@lapometa.com>
2022-07-06 13:45:52 +0200

    feat(redirects) Get redirect from API response - use on SSR and Routing

--- src/pages/[slug].vue
+++ src/pages/[slug].vue
@@ -1,39 +1,66 @@
 <script setup lang="ts">
 import { createError } from 'h3'
 import { useI18n } from 'vue-i18n'
+import { useLocalePath } from 'vue-i18n-routing'
 import { useI18nStore } from '../stores/i18n'
 import type { Page, Post } from '../plugins/content/types'
+const localePath = useLocalePath()
+
+const router = useRouter()
+const { ssrContext, payload } = useNuxtApp()
 
 const route = useRoute()
 const { $content, $seo } = useNuxtApp()
-const { locale } = useI18n()
+const { locale, fallbackLocale } = useI18n()
+
 
 const { slug } = route.params
+const key = `${slug}_${locale.value}`;
 const { data, error } = await useAsyncData<Post | Page>(
-  `${slug}_${locale.value}`,
+  key,
   () => $content.path().slug(slug.toString()).param('lang', locale.value).get()
 )
 
 if (error.value) {
-  const errorCaptured = error.value as any
-  throwError(
-    createError({
-      statusCode: errorCaptured.status,
-      message: JSON.parse(errorCaptured.response.text).message,
-    })
-  )
-}
 
+  delete payload.data[key]
+  delete payload._errors[key]
+
+
+  const errorCaptured = error.value as any
+
+  const redirections = errorCaptured.response.body.redirections || []
+  const redirectFrom = localePath(`/${slug.toString()}`);
+  const redirectTo = redirections.find(redirect => redirect.from === redirectFrom);
+
+  if (redirectTo) {
+
+    if (ssrContext) {
+      const { res } = ssrContext.event
+      res.writeHead(redirectTo.code || 301, { Location: redirectTo.to });
+      res.end();
+    } else {
+      router.push({ path: redirectTo.to })
+    }
+
+  } else {
+    throwError(
+      createError({
+        statusCode: errorCaptured.status,
+        message: JSON.parse(errorCaptured.response.text).message,
+      })
+    )
+  }
+
+
+}
 const i18nStore = useI18nStore()
 
 i18nStore.setRouteParams(data.value.translation)
 
 useHead($seo.getHead(data.value as Post))
+
 </script>
 <template>
-  <component
-    :is="`single-${data.type}`"
-    v-if="data"
-    :data="data"
-  ></component>
+  <component :is="`single-${data.type}`" v-if="data" :data="data"></component>
 </template>
